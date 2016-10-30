125de5e28c87cf995c22a0421a80b42e2fa64663
Maciej Olszewski <hey@maciejolszewski.com>
2016-10-30 14:03:44 +0000

    working mansory

--- app/js/lib/filename.js
+++
@@ -1,1 +0,0 @@
-// library JavaScript file
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
-// user JavaScript file
+$( document ).ready(function() {
+var $grid =  $('.images').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    // use element for option
+    columnWidth: '.grid-width',
+    percentPosition: true
+  })
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });
+});
